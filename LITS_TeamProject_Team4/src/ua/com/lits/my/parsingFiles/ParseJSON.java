115aa92dcf2b610626a1feeea1af6c037c8a5d6a
Olesia Top <olesja.ua@gmail.com>
2015-09-12 23:20:13 +0300

    Added test files and libreries

---
+++ LITS_TeamProject_Team4/src/ua/com/lits/my/parsingFiles/ParseJSON.java
@@ -0,0 +1,12 @@
+package ua.com.lits.my.parsingFiles;
+
+/**
+ * Created by olesia on 12.09.15.
+ */
+public class ParseJSON {
+    public static void main(String [] args){
+        System.out.println("Test commit");
+    }
+
+
+}
